81c6bc16b46cbe7425a0ce54b18221d6d0e326d1
mattshardman <mattshardman@gmail.com>
2019-02-25 15:13:13 +0000

    refactor to make dry

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -36,22 +37,7 @@ function MovieDetails({ movie }) {
   const { id, title, director, metascore, stars } = movie;
   return (
     <Link to={`/movies/${id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard {...movie} />
     </Link>
   );
 }
